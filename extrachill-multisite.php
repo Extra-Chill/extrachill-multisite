62294397ae3e6662274fd448e5e81ad0d7cd5a02
Chris Huber <chubes4@users.noreply.github.com>
2026-01-28 16:55:26 -0600

    Sync version constant with plugin header (1.4.8)

--- extrachill-multisite.php
+++ extrachill-multisite.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'EXTRACHILL_MULTISITE_VERSION', '1.4.7' );
+define( 'EXTRACHILL_MULTISITE_VERSION', '1.4.8' );
 define( 'EXTRACHILL_MULTISITE_PLUGIN_FILE', __FILE__ );
 define( 'EXTRACHILL_MULTISITE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'EXTRACHILL_MULTISITE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
